9a8cc0151fe5dc8c2279591983df03f9ebdbd5d3
Karl Hans Laurits <karl@laurits.dev>
2026-03-25 15:07:40 +0200

    Customize blog starter for Wimpy Dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,8 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://blog.wimpy.dev',
   integrations: [mdx(), sitemap()],
   adapter: cloudflare(),
 });
